41be9523bd7f26bef4dd74b97d25238cb342bf9c
Terre8055 <michaelappiah2018@icloud.com>
2022-12-08 15:04:38 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if executed successfully *
+ * Return: 0 if executed successfully
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
