28af866847736a7c62d2237b73921c1708a58f88
Terre8055 <michaelappiah2018@icloud.com>
2022-12-22 21:29:01 +0000

    chnages commitd

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,13 +14,17 @@ char *_strncat(char *dest, char *src, int n)
 
 	c = 0;
 
-
+	/*find size of dest array*/
 	while (dest[c])
-		c++
+		c++;
 
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
-
+	/*null terminate dest*/
 	dest[c + i] = '\0';
 
 	return (dest);
