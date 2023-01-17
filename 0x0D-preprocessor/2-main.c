185fbf733f66e72783006a8bc25c4ffae4c0d093
Terre8055 <michaelappiah2018@icloud.com>
2023-01-17 00:25:52 +0000

    Rechecked

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
-* main - checks the code
-* _FILE_ - checks for uppercase character
-* Return: 0 if c is uppercase, 0 otherwise
-*
-*/
-
-int main() {
-    /* The __FILE__ macro is a predefined macro in the C standard library
-       that contains the current file's name. */
-    printf("%s\n", __FILE__);
-    /* return 0 to indicate successful execution of the program */
-    return 0;
+ * main - prints the name of file
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
