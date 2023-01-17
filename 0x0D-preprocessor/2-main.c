fff49df7780647a9c661622b41f3c89527379dee
Terre8055 <michaelappiah2018@icloud.com>
2023-01-17 00:14:20 +0000

    comments

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * The __FILE__ macro is a predefined macro
+ * in the C standard library that contains
+ * the current file's name.
+ * return 0 to indicate successful
+ */
 
 int main(void) {
     printf("%s\n", __FILE__);
