bfbe08231f2b5f97cd69617507b92131f26dfdb2
Terre8055 <michaelappiah2018@icloud.com>
2023-02-06 23:14:54 +0000

    Test new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,3 +32,38 @@ int create_file(const char *filename, char *text_content)
 	size = _strlen(text_content);
 	mem = malloc(sizeof(char) * size);
 	if (!mem)
+	{
+		close(fd);
+		return (-1);
+	}
+	w = write(fd, text_content, size);
+	if (w == -1)
+	{
+		close(fd);
+		free(mem);
+		return (-1);
+	}
+	close(fd);
+	free(mem);
+	return (1);
+}
+
+/**
+ * _strlen - len
+ *
+ * @s: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
